7de1853c5966c483584f4033d417c15094e62068
SamuelJ12 <jhe1999@naver.com>
2014-11-19 22:37:45 +0900

    jquery mobile 폼에 맞춰서 코드 변경중

--- WebContent/asset/js/index.js
+++ WebContent/asset/js/index.js
@@ -1,17 +1,10 @@
-var img = new Array("source/service.jpg","source/main.jpg");
+/*var img = new Array("asset/img/service.jpg","asset/img/main.jpg");
 var i=0;
 	
 	function chgimg(){
 		document.images[0].src=img[i++];
 	};
-
-	$(document).ready(function(){ //DOM이 준비되고
-		$('#button').click(function(){ // ID가 toggleButton인 요소를 클릭하면
-			var state = $('#content').css('display'); // state 변수에 ID가 moreMenu인 요소의 display의 속성을 '대입'
-			if(state == 'none'){ // state가 none 상태일경우 
-				$('#content').show(); // ID가 moreMenu인 요소를 show();
-			}else{
-				$('#content').hide(); // ID가 moreMenu인 요소를 hide();			
-			}
-		});
-	});
+*/
+$(document).ready(function(){ //DOM占쏙옙 占쌔븝옙품占�
+	$()
+});
